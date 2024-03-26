9e00c6b474c7ea7a92ffc4fac28891ab02740ee6
deboneil07 <bhattacharjeedeboneil@gmail.com>
2024-03-26 21:33:02 +0530

    contract practice

--- prac.sol
+++ prac.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+contract Test {
+    uint256 public store = 12;
+
+    function status(uint256 data) public {
+        store = data;
+    }
+}
