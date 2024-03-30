031031a2f67c201942986e5de13a14ba5184c940
deboneil07 <bhattacharjeedeboneil@gmail.com>
2024-03-30 21:48:04 +0530

    funcs

--- prac.sol
+++ prac.sol
@@ -3,8 +3,19 @@ pragma solidity 0.8.18;
 
 contract Test {
     uint256 public store = 12;
+    string _name = "hello neil";
 
     function status(uint256 data) public {
         store = data;
     }
+
+    function get() public view returns(string memory) {
+        return _name;
+    }
+
+    function set(string memory name) public returns(string memory) {
+        _name = name;
+        return _name;
+    }
+
 }
